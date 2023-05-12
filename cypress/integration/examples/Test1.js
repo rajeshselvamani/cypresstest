a5f789467684217708d24147d6b4f4a2c7f7d959
Rajesh Selvamani <ajith.raj84@gmail.com>
2023-05-12 22:55:00 +0100

    Cypress first commit

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,8 @@
+describe('First Test suite',()=>{
+    it('First Test case',()=>{
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.wait(500);
+       // cy.screenshot();
+        console.log(Math.PI)
+    })
+})
