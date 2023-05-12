ba25d97f05fbe78e4920e4a0138d95c8cd18bafa
Rajesh Selvamani <ajith.raj84@gmail.com>
2023-05-13 00:52:49 +0100

    Assertion commit

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,12 +1,15 @@
 describe('Second Test suite',()=>{
     it('Second Test case',()=>{
-      //  cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+       cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
        // cy.wait(500);
        // cy.screenshot();
        // console.log(Math.PI)
        // const username = Cypress.env('username')
         const val = Cypress.env('testvar')
         cy.log("val : "+val);
+        cy.log()
+        cy.url().should('include', '/rahulshettyacademy.com/') 
+        cy.url().should('include', '/raj.com/') 
        // cy.log(password);
     })
 })
